b9b341131c86a0b421afee8b87565a60273c9b3d
Alexandr <korotkov34reg@gmail.com>
2018-01-25 15:34:36 +0300

    Update Main.java

--- root/businesslogic/src/main/java/ru/k0r0tk0ff/test/Main.java
+++ root/businesslogic/src/main/java/ru/k0r0tk0ff/test/Main.java
@@ -3,7 +3,8 @@ package ru.k0r0tk0ff.test;
 /**
  * Created by korotkov_a_a on 25.01.2018.
  */
-public class AdditionalLogic {
-
-    public static final String AAAFF = "aaaaaafffffffffff";
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AdditionalLogic.AAAFF);
+    }
 }
